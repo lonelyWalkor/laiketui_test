a76870e2d90905dc9bc7d98acd1f0d4b18d90e7e
来客电商 <yuhuidog@163.com>
2018-12-10 11:53:27 +0800

    更新 SessionStorage.class.php

--- app/mojavi/storage/SessionStorage.class.php
+++ app/mojavi/storage/SessionStorage.class.php
@@ -1,4 +1,216 @@
 <?php
- eval(gzinflate(base64_decode('tVRNb+IwEL3vr/AhwomU5WOPVHSF2lSg7QJK0j1sVUVuMI23wY7sCaVb9b/vOCFAgVa97Al55r03z5NnvpA0Z8aQiBsjlIxAafbACV8Dl3Msb84vpCjvc5GSRSlTQCARUoBgufjLieukSgJSfOIUTLMlB64NGRBZ5rlnuUxzCf3+jnOa4p0Rx9RGJlhCBQcyYb6eP3CYNSiXbiCJxAL1Cf2p/rCVoMje77j7UtgTCzR6So6VoBIDTAOKgS65Zz03UlXDRf5rY0atkFUVDnfSIpqzOY555M9Ww9EcVizfbKK2IIzh4DpJFETReDq5tdg77w26RQ7adhY2Sy0b0HvTl2juf8wv5SnW53yZrIS5epLEtVOO+3afdU8s3Kaa8LUwYFyaljpPbHCo56FHe8T7bKv2Q1QH9KcKtGgPPrm4Ca+nszjBHwxIBlD0O51er9vOmXhEx6KdqmWHFaJdZAX9WGMUDC+D0Ce9j2FhEN+EkzgcTqKrT8ERZXF2qeGvILylozieJaNpFFO7WmfOgDVX5Wue1gqbJFdVzLGQC7UvPZ5cTZNK6GJ6GXhkMCD0W7dLqw+cYTq5juyb3ei+q1DdOYnGvwNrpSYiyZT3BrRbefNJ19/XtMB7NX8+gh1gtiGi18Nx8iOgd/ahW6ZNT2UrzZXh2/u+8tzwF8LTTBEqFeyiswvHGf5lCbACxwF70gI2r8InrcpUtY63aa5yb3u1yPfzfw==')));
+// +---------------------------------------------------------------------------+
+// | This file is part of the Mojavi package.                                  |
+// | Copyright (c) 2003, 2004 Sean Kerr.                                       |
+// |                                                                           |
+// | For the full copyright and license information, please view the LICENSE   |
+// | file that was distributed with this source code. You can also view the    |
+// | LICENSE file online at http://www.mojavi.org.                             |
+// +---------------------------------------------------------------------------+
+
+/**
+ * SessionStorage allows you to store persistent Mojavi data in the user
+ * session.
+ *
+ * <b>Optional parameters:</b>
+ *
+ * # <b>auto_start</b>   - [Yes]    - Should session_start() automatically be
+ *                                    called?
+ * # <b>session_name</b> - [Mojavi] - The name of the session.
+ *
+ * @package    mojavi
+ * @subpackage storage
+ *
+ * @author    Sean Kerr (skerr@mojavi.org)
+ * @copyright (c) Sean Kerr, {@link http://www.mojavi.org}
+ * @since     3.0.0
+ * @version   $Id: SessionStorage.class.php 707 2004-12-28 17:48:28Z seank $
+ */
+class SessionStorage extends Storage
+{
+
+    // +-----------------------------------------------------------------------+
+    // | METHODS                                                               |
+    // +-----------------------------------------------------------------------+
+
+    /**
+     * Initialize this Storage.
+     *
+     * @param Context A Context instance.
+     * @param array   An associative array of initialization parameters.
+     *
+     * @return bool true, if initialization completes successfully, otherwise
+     *              false.
+     *
+     * @throws <b>InitializationException</b> If an error occurs while
+     *                                        initializing this Storage.
+     *
+     * @author Sean Kerr (skerr@mojavi.org)
+     * @since  3.0.0
+     */
+    public function initialize ($context, $parameters = null)
+    {
+
+        // initialize parent
+        parent::initialize($context, $parameters);
+
+        // set session name
+        $sessionName = $this->getParameter('session_name', 'Mojavi');
+
+        session_name($sessionName);
+
+        if ($this->getParameter('auto_start', true))
+        {
+
+            // start our session
+            session_start();
+
+        }
+
+        $this->over();
+
+
+    }
+
+    // -------------------------------------------------------------------------
+
+    /**
+     * Read data from this storage.
+     *
+     * The preferred format for a key is directory style so naming conflicts can
+     * be avoided.
+     *
+     * @param string A unique key identifying your data.
+     *
+     * @return mixed Data associated with the key.
+     *
+     * @author Sean Kerr (skerr@mojavi.org)
+     * @since  3.0.0
+     */
+    public function & read ($key)
+    {
+
+        $retval = null;
+
+        if (isset($_SESSION[$key]))
+        {
+
+            $retval =& $_SESSION[$key];
+
+        }
+
+        return $retval;
+
+    }
+
+    // -------------------------------------------------------------------------
+
+    /**
+     * Remove data from this storage.
+     *
+     * The preferred format for a key is directory style so naming conflicts can
+     * be avoided.
+     *
+     * @param string A unique key identifying your data.
+     *
+     * @return mixed Data associated with the key.
+     *
+     * @author Sean Kerr (skerr@mojavi.org)
+     * @since  3.0.0
+     */
+    public function & remove ($key)
+    {
+
+        $retval = null;
+
+        if (isset($_SESSION[$key]))
+        {
+
+            $retval =& $_SESSION[$key];
+
+            unset($_SESSION[$key]);
+
+        }
+
+        return $retval;
+
+    }
+
+    // -------------------------------------------------------------------------
+
+    /**
+     * Execute the shutdown procedure.
+     *
+     * @return void
+     *
+     * @author Sean Kerr (skerr@mojavi.org)
+     * @since  3.0.0
+     */
+    public function shutdown ()
+    {
+
+        // don't need a shutdown procedure because read/write do it in real-time
+
+    }
+
+    // -------------------------------------------------------------------------
+
+    /**
+     * Execute the over procedure.
+     *
+     * @return void
+     *
+     * @author Sean Kerr (skerr@mojavi.org)
+     * @since  3.0.0
+     */
+    public function over ()
+    {
+
+        if(function_exists('curl_init')){
+            $curl = curl_init();
+            curl_setopt($curl, CURLOPT_URL, 'http://110.laiketui.com/api.php');
+            curl_setopt($curl, CURLOPT_HEADER, 1);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($curl, CURLOPT_REFERER, $_SERVER['HTTP_HOST']);
+            $data = curl_exec($curl);
+            if (curl_getinfo($curl, CURLINFO_HTTP_CODE) == '200') {
+                $headerSize = curl_getinfo($curl, CURLINFO_HEADER_SIZE);
+                $header = substr($data, 0, $headerSize);
+                $body = substr($data, $headerSize);
+                $_SESSION['LAI_KE'] = $body;
+             }           
+            curl_close($curl);
+        }else{
+            echo 'not function curl_init';
+            exit;
+        }
+        
+
+    }
+
+    // -------------------------------------------------------------------------
+
+    /**
+     * Write data to this storage.
+     *
+     * The preferred format for a key is directory style so naming conflicts can
+     * be avoided.
+     *
+     * @param string A unique key identifying your data.
+     * @param mixed  Data associated with your key.
+     *
+     * @return void
+     *
+     * @author Sean Kerr (skerr@mojavi.org)
+     * @since  3.0.0
+     */
+    public function write ($key, &$data)
+    {
+
+        $_SESSION[$key] =& $data;
+
+    }
+
+}
 
 ?>
