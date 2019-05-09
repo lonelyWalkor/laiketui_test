07491e5a3c1d7f1bffe3cb9d17686a1c76396d20
dandan <1970935666@qq.com>
2019-05-09 17:03:21 +0800

    修改后台签到页面 活动时间过期后状态改变

--- app/LKT/webapp/modules/api/actions/signAction.class.php
+++ app/LKT/webapp/modules/api/actions/signAction.class.php
@@ -207,71 +207,75 @@ class signAction extends Action {
             // 查询正在进行的签到活动
             $sql = "select * from lkt_sign_activity where status = 1";
             $rrr = $db->select($sql);
+           
             if($rrr){
                 $starttime = date("Ymd",strtotime($rrr[0]->starttime)); // 开始时间
                 $endtime = date("Ymd",strtotime($rrr[0]->endtime)); // 结束时间
                 $details = $rrr[0]->detail;
 
                 $day = $endtime - $starttime; // 活动天数
-            }else{
-                $day = 1;
-            }
+                //------------------
+                $start_1 = date("Y-m-d 00:00:00",strtotime("-1 day")); // 昨天开始时间
+                $end_1 = date("Y-m-d 23:59:59",strtotime("-1 day")); // 昨天结束时间
+                $num = 0;
+                // 根据用户id, 查询昨天签到记录
+                $sql = "select * from lkt_sign_record where user_id = '$user_id' and sign_time > '$start_1' and sign_time < '$end_1' and type = 0";
+                $rrrr = $db->select($sql);
+                if($rrrr){ // 有数据,就循环查询连续签到几天
+                    for ($i=1; $i <= $day; $i++) {
+                        $start = date("Y-m-d 00:00:00",strtotime("-$i day"));
+                        $end = date("Y-m-d 23:59:59",strtotime("-$i day"));
 
-            $start_1 = date("Y-m-d 00:00:00",strtotime("-1 day")); // 昨天开始时间
-            $end_1 = date("Y-m-d 23:59:59",strtotime("-1 day")); // 昨天结束时间
-            $num = 0;
-            // 根据用户id, 查询昨天签到记录
-            $sql = "select * from lkt_sign_record where user_id = '$user_id' and sign_time > '$start_1' and sign_time < '$end_1' and type = 0";
-            $rrrr = $db->select($sql);
-            if($rrrr){ // 有数据,就循环查询连续签到几天
-                for ($i=1; $i <= $day; $i++) {
-                    $start = date("Y-m-d 00:00:00",strtotime("-$i day"));
-                    $end = date("Y-m-d 23:59:59",strtotime("-$i day"));
-
-                    $sql = "select * from lkt_sign_record where user_id = '$user_id' and sign_time > '$start' and sign_time < '$end' and type = 0";
-                    $r_num = $db->select($sql);
-                    if(empty($r_num)){
-                        $num = $i - 1;
-                        break;
+                        $sql = "select * from lkt_sign_record where user_id = '$user_id' and sign_time > '$start' and sign_time < '$end' and type = 0";
+                        $r_num = $db->select($sql);
+                        if(empty($r_num)){
+                            $num = $i - 1;
+                            break;
+                        }
                     }
                 }
-            }
 
-            $startdate = date("$year-$month-01 00:00:00", strtotime(date("Y-m-d"))); // 月开始时间
-            $enddate = date('Y-m-d 23:59:59', strtotime("$startdate +1 month -1 day")); // 月结束时间
+                $startdate = date("$year-$month-01 00:00:00", strtotime(date("Y-m-d"))); // 月开始时间
+                $enddate = date('Y-m-d 23:59:59', strtotime("$startdate +1 month -1 day")); // 月结束时间
 
-            $y_time = date('Y',strtotime( date("Y-m-d" ))); // 本年年份
-            $m_time = date('m',strtotime( date("Y-m-d" ))); // 本月月份
-            if($m_time < 10){
-                $m_time = str_replace ("0", "", $m_time);
-            }
-            if($year > $y_time || $month > $m_time){
-                echo json_encode(array('status'=>1,'sign_time'=>'','imgurl'=>$imgurl,'num'=>$num,'details'=>$details));
-                exit;
-            }
-            $time1 = date('Y-m',time());
-
-            $sign_time = [];
-            $sql = "select sign_time from lkt_sign_record where user_id = '$user_id' and sign_time like '$time1%' and type = 0";
-            $r_2 = $db->select($sql);
-            // var_dump($sql,$r_2);
-            if($r_2){
-                foreach ($r_2 as $k => $v) {
-                    $y = date("Y",strtotime($v->sign_time));
-                    $m = date("m",strtotime($v->sign_time));
-                    $d = date("d",strtotime($v->sign_time));
-                    if($m < 10){
-                        $m = str_replace ("0", "", $m);
-                    }
-                    if($d < 10){
-                        $d = str_replace ("0", "", $d);
-                    }
-                    $sign_time[$k] = $y . $m . $d;
+                $y_time = date('Y',strtotime( date("Y-m-d" ))); // 本年年份
+                $m_time = date('m',strtotime( date("Y-m-d" ))); // 本月月份
+                if($m_time < 10){
+                    $m_time = str_replace ("0", "", $m_time);
                 }
-                echo json_encode(array('status'=>1,'sign_time'=>$sign_time,'imgurl'=>$imgurl,'num'=>$num,'details'=>$details));
-                exit;
+                if($year > $y_time || $month > $m_time){
+                    echo json_encode(array('status'=>1,'sign_time'=>'','imgurl'=>$imgurl,'num'=>$num,'details'=>$details));
+                    exit;
+                }
+                $time1 = date('Y-m',time());
+
+                $sign_time = [];
+                $sql = "select sign_time from lkt_sign_record where user_id = '$user_id' and sign_time like '$time1%' and type = 0";
+                $r_2 = $db->select($sql);
+                // var_dump($sql,$r_2);
+                if($r_2){
+                    foreach ($r_2 as $k => $v) {
+                        $y = date("Y",strtotime($v->sign_time));
+                        $m = date("m",strtotime($v->sign_time));
+                        $d = date("d",strtotime($v->sign_time));
+                        if($m < 10){
+                            $m = str_replace ("0", "", $m);
+                        }
+                        if($d < 10){
+                            $d = str_replace ("0", "", $d);
+                        }
+                        $sign_time[$k] = $y . $m . $d;
+                    }
+                    echo json_encode(array('status'=>1,'sign_time'=>$sign_time,'imgurl'=>$imgurl,'num'=>$num,'details'=>$details));
+                    exit;
+                }else{
+                    echo json_encode(array('status'=>0,'err'=>'暂无签到记录！','num'=>0,'details'=>$details));
+                    exit;
+                }
+                //------------------------
             }else{
-                echo json_encode(array('status'=>0,'err'=>'暂无签到记录！','num'=>0,'details'=>$details));
+               // $day = 1;
+                echo json_encode(array('status'=>2,'err'=>'活动已结束！'));
                 exit;
             }
         }else{
