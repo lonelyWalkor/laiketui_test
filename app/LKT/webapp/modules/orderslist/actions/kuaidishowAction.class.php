9e506274c029be804b8e62e7fe4cf5c10e36634f
dandan <1970935666@qq.com>
2019-05-10 17:53:33 +0800

    后台查看物流信息接口修改

--- app/LKT/webapp/modules/orderslist/actions/kuaidishowAction.class.php
+++ app/LKT/webapp/modules/orderslist/actions/kuaidishowAction.class.php
@@ -22,15 +22,18 @@ class kuaidishowAction extends Action {
         // 根据订单详情id,修改订单详情
             $sql = "select express_id,courier_num from lkt_order_details where r_sNo = '$r_sNo'";
             $r = $db->select($sql);
+
         if(!empty($r[0]->express_id) && !empty($r[0]->courier_num)){
             $express_id = $r[0]->express_id;//快递公司ID
             $courier_num = $r[0]->courier_num;//快递单号
             $sql01 = "select * from lkt_express where id = '$express_id'";
             $r01 = $db->select($sql01);
+
             $type = $r01[0]-> type;//快递公司代码
             $kuaidi_name = $r01[0]-> kuaidi_name;
-            $url = "http://www.kuaidi100.com/query?type=$type&postid=$courier_num";
-            $res = $this->httpsRequest($url);
+           
+
+            $res = $this-> logistics2($type,$courier_num);
 
             $res_1 = json_decode($res);
             if (empty($res_1->data)) {
@@ -50,26 +53,40 @@ class kuaidishowAction extends Action {
 	}
 
 	
-    function httpsRequest($url, $data=null) {
-            // 1.初始化会话
-            $ch = curl_init();
-            // 2.设置参数: url + header + 选项
-            // 设置请求的url
-            curl_setopt($ch, CURLOPT_URL, $url);
-            // 保证返回成功的结果是服务器的结果
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            if(!empty($data)) {
-                // 发送post请求
-                curl_setopt($ch, CURLOPT_POST, 1);
-                // 设置发送post请求参数数据
-                curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-            }
-            // 3.执行会话; $result是微信服务器返回的JSON字符串
-            $result = curl_exec($ch);
-            // 4.关闭会话
-            curl_close($ch);
-            return $result;
-    
+    public static function logistics2($type,$courier_num){
+        $url = 'http://poll.kuaidi100.com/poll/query.do'; //实时查询
+        $key = 'suLyYnWs4118';           //客户授权key
+        $customer = '96EB664A39B315C5C81294793F7EEC19'; //查询公司编号
+        $param = array (
+            'com' =>$type.'',             //快递公司编码
+            'num' => trim($courier_num),      //快递单号
+            'phone' => '',              //手机号
+            'from' => '',               //出发地城市
+            'to' => '',                 //目的地城市
+            'resultv2' => '1'           //开启行政区域解析
+        );
+        // var_dump($param);die;
+        //请求参数
+        $post_data = array();
+        $post_data["customer"] = $customer;
+        $post_data["param"] = json_encode($param);
+        $sign = md5($post_data["param"].$key.$post_data["customer"]);
+        $post_data["sign"] = strtoupper($sign);
+        $params = "";
+        foreach ($post_data as $k=>$v) {
+            $params .= "$k=".urlencode($v)."&";     //默认UTF-8编码格式
+        }
+        $post_data = substr($params, 0, -1);
+        // $res = HttpTools::httpsRequest($url,$post_data);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch,CURLOPT_POSTFIELDS,$post_data);
+        curl_setopt($ch, CURLOPT_TIMEOUT,3);
+        $result = curl_exec($ch);
+        return $result;   
+ 
+       
     }
 	public function getRequestMethods() {
 		return Request::POST;
