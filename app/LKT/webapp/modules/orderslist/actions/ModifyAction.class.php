35b42fc7534b5c8671a185aea4df49e6a3cb63bc
疯子技师 <yuhuidog@163.com>
2019-11-12 13:07:58 +0800

    优化：后台退货管理

--- app/LKT/webapp/modules/orderslist/actions/ModifyAction.class.php
+++ app/LKT/webapp/modules/orderslist/actions/ModifyAction.class.php
@@ -55,41 +55,24 @@ class ModifyAction extends Action {
 
       foreach ($res as $k => $v) { 
         $sid = $v -> sid;
-        $freight=$freight+$v -> freight;
-        
+        $freight=$freight+$v -> freight;        
         $data['user_name'] = $v -> user_name; // 联系人
         $data['name'] = $v -> name; // 联系人
-
         $data['sNo'] = $v -> sNo; // 订单号
-
         $data['mobile'] = $v -> mobile; // 联系电话
-
         $data['address'] = $v -> address; // 详细地址
-
         $data['add_time'] = $v -> add_time; // 添加时间
-
         $data['z_price'] = $v -> z_price; // 
-
         $data['user_id'] = $v -> user_id; // 用户id
-
         $data['deliver_time'] = $v -> deliver_time; // 发货时间
-
         $data['arrive_time'] = $v -> arrive_time; // 到货时间
-
         $data['r_status'] = $v -> r_status; // 订单详情状态
-
         $data['status01'] = $v -> r_status; // 订单详情状态
-
         $data['gstatus'] = $v -> status; // 订单详情状态
-
         $data['otype'] = $v -> otype;  // 订单类型
-
         $data['content'] = $v -> content; // 退货原因
-
         $data['express_id'] = $v -> express_id; // 快递公司id
-
         $data['courier_num'] = $v -> courier_num; // 快递单号
-
         $data['drawid'] = $v -> drawid; // 抽奖ID
         $data['coupon_price'] = $v -> coupon_price; // 快递单号
         $reduce_price = $v -> reduce_price; // 满减金额
@@ -97,11 +80,9 @@ class ModifyAction extends Action {
         $allow = $v -> allow; // 积分
 
         $data['paytype'] = $v -> pay; // 支付方式
-
         $data['trade_no'] = $v -> trade_no; // 微信支付交易号
         $data['freight'] = $v -> freight; // 运费
         $data['id'] = $id;
-
         $exper_id = $v -> express_id;
         if($exper_id){
             $r03 =$db->select("select * from lkt_express where id = $exper_id ");
@@ -112,46 +93,29 @@ class ModifyAction extends Action {
         }
         $courier_num111[$k]['kuaidi_name'] = $res[$k] -> kuaidi_name;
         $courier_num111[$k]['courier_num'] = $v->courier_num;
-        // print_r($data);die;
         // 根据产品id,查询产品主图
         $psql = 'select imgurl,product_title from lkt_product_list where id="'.$v -> p_id.'"';
         $img = $db -> selectarray($psql);
-
         if(!empty($img)){
           $v -> pic = $img[0]['imgurl'];
           $v -> p_name = $img[0]['product_title'];
           $res[$k] = $v;
         }
-
         $res[$k] -> z_price = $v -> num * $v -> p_price;
         $z_price =$z_price+$v -> num * $v -> p_price;
         $user_id= $v -> user_id; // 用户id
-
         $drawid= $v -> drawid; // 抽奖ID
-
         $add_time = $v -> add_time; // 添加时间
-
         if(!empty($drawid)&& $drawid !=0){
 
           $sql07 = "select * from lkt_draw_user where id = '$drawid'  ";
-
           $r07 =$db->select($sql07);
-
           if(!empty($r07)){
-
-              $lottery_status = $r07[0]->lottery_status;
-
-            // print_r($r07);die;
-
-             $data['lottery_status'] = $lottery_status;
-
+            $lottery_status = $r07[0]->lottery_status;
+            $data['lottery_status'] = $lottery_status;
            }else{
-
               $data['lottery_status'] = 7;
-
-           }
-
-          
+           }   
 
         }else{
 
@@ -174,12 +138,11 @@ class ModifyAction extends Action {
                                 $tmp_arr[] = $v1['courier_num'];
                             }
                     }
-                    
                 }
 
                 sort($arr);
                 $courier_num111=$arr;
-            }
+      }
 
 
     $data['courier_num'] = $courier_num111; // 快递单号
@@ -189,168 +152,97 @@ class ModifyAction extends Action {
     
 
    if($data['otype'] == 'pt'){
-
       switch ($data['gstatus']) {
-
             case 0:
-
               $data['r_status'] ='未付款';
-
               break;
-
             case 9:
-
               $data['r_status'] ='拼团中';
-
               break;
-
             case 1:
-
               $data['r_status'] ='拼团成功-未发货';
-
               break;
-
             case 2:
-
               $data['r_status'] ='拼团成功-已发货';
-
               break;
-
             case 3:
-
               $data['r_status'] ='拼团成功-已签收';
-
               break;
-
             case 10:
-
               $data['r_status'] ='拼团失败-未退款';
-
               break;
-
             case 11:
-
               $data['r_status'] ='拼团失败-已退款';
-
               break;
-
             }
 
    }else{
-
        if($data['gstatus'] == 0){
-
-        $data['r_status'] = '未付款';
-
-      }else if($data['gstatus'] == 1){
-
-        $data['r_status'] = '未发货';
-
+            $data['r_status'] = '未付款';
+      }else if($data['gstatus'] == 1){  
+            $data['r_status'] = '未发货';
       }else if($data['gstatus'] == 2){
-
-        $data['r_status'] = '已发货';
-
+            $data['r_status'] = '已发货';
       }else if($data['gstatus'] == 3){
-
-        $data['r_status'] = '待评论';
-
+            $data['r_status'] = '待评论';
       }else if($data['gstatus'] == 4){
-
-        $data['r_status'] = '退货';
-
+            $data['r_status'] = '退货';
       }else if($data['gstatus'] == 5){
-
-        $data['r_status'] = '已完成';
-
+            $data['r_status'] = '已完成';
       }else if($data['gstatus'] == 6){
-
-        $data['r_status'] = '订单关闭';
-
+            $data['r_status'] = '订单关闭';
       }else if($data['gstatus'] == 12){
-
-        $data['r_status'] = '已完成';
+            $data['r_status'] = '已完成';
 
       }
 
     }
 
       $status = 0;
-
-    
-
       $sNo = trim($request -> getParameter('sNo'));
-
       $trade = intval($request -> getParameter('trade'))-1;
-
       $express_id = $request -> getParameter('kuaidi'); // 快递公司id
-
       $courier_num = $request -> getParameter('danhao'); // 快递单号
-
       $freight = $request -> getParameter('yunfei'); // 运费
-
       $time = date('Y-m-d h:i:s',time());
 
-
-
       if(!empty($sNo)&&!empty($trade)){
-
         $sql01 = "select * from lkt_order where sNo = '$sNo' ";
-// print_r($sql01);die;
         $r01 =$db->select($sql01);
-
-
         $data['status01'] = $r01[0]->status; // 根据订单号查询该订单的状态
-
       }
-
-
 
       $sqll = 'update lkt_order set status='.$trade.' where sNo="'.$sNo.'"';
-
       $rl = $db -> update($sqll);
-
-
-
       $sqld = 'update lkt_order_details set r_status='.$trade.'  where r_sNo="'.$sNo.'"';
-
       $rd = $db -> update($sqld);
 
-
-
       if($rl>0 && $rd>0){
-
         echo json_encode(array('status' => 1,'msg' => '操作成功!'));exit();
-
       }
 
-
-
       $sql02 = "select * from lkt_express ";
-
       $r02 =$db->select($sql02);
-
 //佣金信息
       $sqllud = "select a.*,b.user_name,b.headimgurl from lkt_distribution_record as a ,lkt_user as b  where a.sNo = ".$data['sNo']." and a.level >0  and a.user_id = b.user_id order by level asc";
-            $rlud = $db -> select($sqllud);
-            if(empty($rlud)){
+      $rlud = $db -> select($sqllud);
+      if(empty($rlud)){
                     $request -> setAttribute("fenxiaoshang",1);
             }else{
                   foreach ($rlud as $keyl => $valuel) {
-                  $user_id = $valuel->user_id;
-                  $sqlludd = "select user_name,headimgurl from lkt_user where user_id = '$user_id' ";
-                  $rludd = $db -> select($sqlludd);
-                  
-                  $dd[$keyl]['level'] = $valuel->level;
-                  $dd[$keyl]['money'] = $valuel->money;
-                  $dd[$keyl]['user_name'] = $rludd[0]->user_name;
-                  $dd[$keyl]['headimgurl'] = $rludd[0]->headimgurl;
-                  $request -> setAttribute("fenxiaoshang",$dd);
+                    $user_id = $valuel->user_id;
+                    $sqlludd = "select user_name,headimgurl from lkt_user where user_id = '$user_id' ";
+                    $rludd = $db -> select($sqlludd);
+                    $dd[$keyl]['level'] = $valuel->level;
+                    $dd[$keyl]['money'] = $valuel->money;
+                    $dd[$keyl]['user_name'] = $rludd[0]->user_name;
+                    $dd[$keyl]['headimgurl'] = $rludd[0]->headimgurl;
+                    $request -> setAttribute("fenxiaoshang",$dd);
                 }
-            }
+      }
           $allow = 0; // 积分
           $sql02 = "select * from lkt_express ";
           $r02 = $db -> select($sql02);
-          // print_r($data);die;
           $request -> setAttribute("uploadImg",$uploadImg);
           $request -> setAttribute("data",$data);
           $request -> setAttribute("detail",$res);
@@ -366,32 +258,29 @@ class ModifyAction extends Action {
     }
        
   public function execute(){
-     $db = DBAction::getInstance();
-     $request = $this->getContext()->getRequest();
-     
+      $db = DBAction::getInstance();
+      $request = $this->getContext()->getRequest();
       $m = $request -> getParameter('m'); // 订单id
-     if($m){
+      if($m){
+        $this->$m();
+      }
+       $admin_id = $this->getContext()->getStorage()->read('admin_id');
+       $name = addslashes(trim($request->getParameter('name')));
+       $mobile = addslashes(trim($request->getParameter('mobile')));
+       $sheng = addslashes(trim($request->getParameter('Select1')));
+       $shi = addslashes(trim($request->getParameter('Select2')));
+       $xian = addslashes(trim($request->getParameter('Select3')));
+       $address = addslashes(trim($request->getParameter('address')));
+       $z_price = addslashes(trim($request->getParameter('z_price')));
+       $r1 = $db -> selectarray('select G_CName from admin_cg_group where GroupID='.$sheng);
+       $r1 = $r1[0]['G_CName'];
+       $r2 = $db -> selectarray('select G_CName from admin_cg_group where GroupID='.$shi);
+       $r2 = $r2[0]['G_CName'];
+       $r3 = $db -> selectarray('select G_CName from admin_cg_group where GroupID='.$xian);
+       $r3 = $r3[0]['G_CName'];
 
-       $this->$m();
-     }
-     $admin_id = $this->getContext()->getStorage()->read('admin_id');
-
-     $name = addslashes(trim($request->getParameter('name')));
-     $mobile = addslashes(trim($request->getParameter('mobile')));
-     $sheng = addslashes(trim($request->getParameter('Select1')));
-     $shi = addslashes(trim($request->getParameter('Select2')));
-     $xian = addslashes(trim($request->getParameter('Select3')));
-     $address = addslashes(trim($request->getParameter('address')));
-     $z_price = addslashes(trim($request->getParameter('z_price')));
-     $r1 = $db -> selectarray('select G_CName from admin_cg_group where GroupID='.$sheng);
-     $r1 = $r1[0]['G_CName'];
-     $r2 = $db -> selectarray('select G_CName from admin_cg_group where GroupID='.$shi);
-     $r2 = $r2[0]['G_CName'];
-     $r3 = $db -> selectarray('select G_CName from admin_cg_group where GroupID='.$xian);
-     $r3 = $r3[0]['G_CName'];
-
-     $address = $r1.$r2.$r3.$address;
-     $sNo = addslashes(trim($request->getParameter('id')));
+       $address = $r1.$r2.$r3.$address;
+       $sNo = addslashes(trim($request->getParameter('id')));
        $sid = addslashes(trim($request->getParameter('sid')));
 
      $sql = 'update lkt_order set z_price='.$z_price.',name="'.$name.'",mobile="'.$mobile.'",address="'.$address.'" where sNo="'.$sNo.'"';     
@@ -417,31 +306,31 @@ class ModifyAction extends Action {
      $n_price = $request -> getParameter('n_price'); // 现价
 
      if($y_price && $n_price && $id){
-      $price = $n_price-$y_price;
-      $rlud = $db -> select("select r_sNo from lkt_order_details where id ='$id'" );
+          $price = $n_price-$y_price;
+          $rlud = $db -> select("select r_sNo from lkt_order_details where id ='$id'" );
 
-      $sqld = 'update lkt_order_details set p_price='.$n_price.'  where id="'.$id.'"';
-      $rd = $db -> update($sqld);
-        if($rlud){
-          $sNo = $rlud[0]->r_sNo;
-           if($price>=0){//提高价格
-              $sqll = 'update lkt_order set z_price=z_price+'."$price".' where sNo="'.$sNo.'"';
-              $rl = $db -> update($sqll);
+          $sqld = 'update lkt_order_details set p_price='.$n_price.'  where id="'.$id.'"';
+          $rd = $db -> update($sqld);
+              if($rlud){
+                $sNo = $rlud[0]->r_sNo;
+                 if($price>=0){//提高价格
+                    $sqll = 'update lkt_order set z_price=z_price+'."$price".' where sNo="'.$sNo.'"';
+                    $rl = $db -> update($sqll);
+                  }else{//降低价格
+                    $sqll = 'update lkt_order set z_price=z_price'."$price".' where sNo="'.$sNo.'"';
+                    $rl = $db -> update($sqll);
+                  }
 
-            }else{//降低价格
-              $sqll = 'update lkt_order set z_price=z_price'."$price".' where sNo="'.$sNo.'"';
-              // echo $sqll;
-              $rl = $db -> update($sqll);
-            }
-            echo json_encode(array('status'=>1,'err'=>'修改成功!'));
-                exit();
-        }else{
-          echo json_encode(array('status'=>0,'err'=>'参数错误1!'));
-                exit();
-        }
+                  echo json_encode(array('status'=>1,'err'=>'修改成功!'));
+                  exit();
+              }else{
+                  echo json_encode(array('status'=>0,'err'=>'参数错误!'));
+                  exit();
+              }
+
      }else{
-      echo json_encode(array('status'=>0,'err'=>'参数错误!'));
-                exit();
+            echo json_encode(array('status'=>0,'err'=>'参数错误!'));
+            exit();
      }
 
 
