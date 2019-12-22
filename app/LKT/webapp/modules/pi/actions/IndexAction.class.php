bf1bc6b5036f1d19f792c07c7b2be1e46b5fb468
疯子技师 <yuhuidog@163.com>
2019-12-22 08:50:31 +0800

    新增：后台插件框架搭建

---
+++ app/LKT/webapp/modules/pi/actions/IndexAction.class.php
@@ -0,0 +1,37 @@
+<?php
+require_once(MO_LIB_DIR . '/DBAction.class.php');
+require_once(MO_LIB_DIR . '/ShowPager.class.php');
+require_once(MO_LIB_DIR . '/Tools.class.php');
+
+class IndexAction extends Action {
+
+    public function getDefaultView() {
+        $request = $this->getContext()->getRequest();
+        $methodName = addslashes(trim($request->getParameter('m'))); //调用哪个方法
+        $className = addslashes(trim($request->getParameter('c'))); //调用哪个类文件
+        $pluginName = addslashes(trim($request->getParameter('p'))); //插件名称，文件名
+        if($pluginName){
+            require_once(MO_WEBAPP_DIR."/plugins/".$pluginName."/admin/".$className.".class.php");
+            $plugin = new $className($this->getContext());
+            if ($methodName) {
+                $plugin->$methodName();
+            }else{
+                $plugin->execute();
+            }
+
+        }
+
+        return View :: INPUT;
+    }
+
+    public function execute() {
+
+    }
+
+    public function getRequestMethods(){
+        return Request :: NONE;
+    }
+
+}
+
+?>
