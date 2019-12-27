4b9f575879f29cad03d8925255a385aadb63ca6a
疯子技师 <yuhuidog@163.com>
2019-12-27 20:07:30 +0800

    修复：后台插件缓存导致的页面一致问题

--- app/LKT/webapp/modules/pi/views/IndexInputView.class.php
+++ app/LKT/webapp/modules/pi/views/IndexInputView.class.php
@@ -7,10 +7,17 @@
 require_once(MO_WEBAPP_DIR . "/plugins/PluginInputView.class.php");
 class IndexInputView extends SmartyView {
     public function execute() {
-		$request = $this->getContext()->getRequest();
+        $request = $this->getContext()->getRequest();
         $className = $request->getAttribute("c") ; //调用哪个类文件
         $pluginName = $request->getAttribute("p") ; //插件名称，文件名
         if($pluginName){
+            $path = MO_WEBAPP_DIR."/_compile/pi/";
+            $p = scandir($path);
+            foreach ($p as $val) {
+                if ($val != "." && $val != "..") {
+                    unlink($path . $val);
+                }
+            }
             require_once(MO_WEBAPP_DIR."/plugins/".$pluginName."/admin/views/".$className."InputView.class.php");
             $className = $className."InputView";
             $pluginview = new $className($this,$pluginName);
