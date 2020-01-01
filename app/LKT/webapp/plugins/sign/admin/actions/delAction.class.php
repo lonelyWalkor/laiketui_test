038408d50e4210c19e340c06297c0b4bf516d790
疯子技师 <yuhuidog@163.com>
2020-01-01 10:56:06 +0800

    修复：签到插件活动添加异常

--- app/LKT/webapp/plugins/sign/admin/actions/delAction.class.php
+++ app/LKT/webapp/plugins/sign/admin/actions/delAction.class.php
@@ -7,27 +7,28 @@
 class delAction extends PluginAction {
 
     public function getDefaultView() {
-        return;
-    }
-
-    public function execute(){
         $db = DBAction::getInstance();
         $request = $this->getContext()->getRequest();
         // 接收信息
         $id = intval($request->getParameter('id')); // 插件id
         $uploadImg = addslashes(trim($request->getParameter('uploadImg'))); // 图片上传位置
-        
+
 
         $sql = "select * from lkt_sign_activity where id=".$id;
         $r = $db->select($sql);
         $image = $r[0]->image;
         @unlink ($uploadImg.$image);
-        
+
         $sql = 'delete from lkt_sign_activity where id='.$id;
         $res = $db -> delete($sql);
         if($res > 0){
             jump("index.php?module=pi&p=sign","删除成功!");
         }
+        return;
+    }
+
+    public function execute(){
+
         
     }
 
